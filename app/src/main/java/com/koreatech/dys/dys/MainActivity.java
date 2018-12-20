5f0b9d242424159884dc5236cc72488e4353ba80
0sck0 <kimsc0714@gmail.com>
2018-12-21 00:33:53 +0900

    MainFragment added

--- app/src/main/java/com/koreatech/dys/dys/MainActivity.java
+++ app/src/main/java/com/koreatech/dys/dys/MainActivity.java
@@ -1,18 +1,14 @@
 package com.koreatech.dys.dys;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
@@ -39,15 +35,6 @@ public class MainActivity extends AppCompatActivity
         transaction.addToBackStack(null);
         transaction.commit();
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -65,33 +52,16 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            for (Fragment fragment: getSupportFragmentManager().getFragments()) {
+                if (fragment.isVisible()) {
+                    if(fragment instanceof MainFragment)
+                        finish();
+                    else
+                        super.onBackPressed();
+                }
+            }
         }
     }
-
-    // 우측 상단 메뉴 구현 메소드
-    // 메인 메뉴에는 Navigation drawer가 메뉴 기능을 대신 하므로 메뉴를 사용하지 않음
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
     // Navigation drawer 메뉴 선택 시 프래그먼트 이동 메소드
     @SuppressWarnings("StatementWithEmptyBody")
--- app/src/main/java/com/koreatech/dys/dys/MainFragment.java
+++ app/src/main/java/com/koreatech/dys/dys/MainFragment.java
@@ -1,14 +1,15 @@
 package com.koreatech.dys.dys;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -17,6 +18,18 @@ public class MainFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    // 프래그먼트 선언
+    private Fragment timeFragment;
+    private Fragment creditFragment;
+
+    // 각 버튼(FrameLayout) 선언
+    FrameLayout schedule;
+    FrameLayout timetable;
+    FrameLayout studyPlanner;
+    FrameLayout reminder;
+    FrameLayout lab;
+    FrameLayout settings;
 
     public MainFragment() {
         // Required empty public constructor
@@ -44,14 +57,97 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        timeFragment = new TimetableFragment();
+        creditFragment = new CreditFragment();
+
+        schedule = (FrameLayout) view.findViewById(R.id.tile_schedule);
+        timetable = (FrameLayout) view.findViewById(R.id.tile_timetable);
+        studyPlanner = (FrameLayout) view.findViewById(R.id.tile_study_planner);
+        reminder = (FrameLayout) view.findViewById(R.id.tile_reminder);
+        lab = (FrameLayout) view.findViewById(R.id.tile_lab);
+        settings = (FrameLayout) view.findViewById(R.id.tile_settings);
+
+        schedule.setOnClickListener(this);
+        timetable.setOnClickListener(this);
+        studyPlanner.setOnClickListener(this);
+        reminder.setOnClickListener(this);
+        lab.setOnClickListener(this);
+        settings.setOnClickListener(this);
+
+//        schedule.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+//        timetable.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+//
+//                transaction.replace(R.id.container, timeFragment);
+//                transaction.addToBackStack(null);
+//                transaction.commit();
+//            }
+//        });
+//        studyPlanner.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+//        reminder.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+//        lab.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+//        settings.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+
         getActivity().setTitle(R.string.title_main);
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
     }
 
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        if (id == R.id.tile_schedule) {
+
+        } else if (id == R.id.tile_timetable) {
+            transaction.replace(R.id.container, timeFragment);
+        } else if (id == R.id.tile_study_planner) {
+
+        } else if (id == R.id.tile_reminder) {
+
+        } else if (id == R.id.tile_lab) {
+
+        } else if (id == R.id.tile_settings) {
+
+        }
+
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
